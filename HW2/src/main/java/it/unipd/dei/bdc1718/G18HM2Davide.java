d45830d862eafacf8e6b879c46cac9f2b173450a
Davide Talon <talon.davide@gmail.com>
2018-04-09 23:06:45 +0200

    Random key fixed

--- HW2/src/main/java/it/unipd/dei/bdc1718/G18HM2Davide.java
+++ HW2/src/main/java/it/unipd/dei/bdc1718/G18HM2Davide.java
@@ -35,8 +35,13 @@ public class G18HM2Davide {
             .setAppName("Second homework");
     JavaSparkContext sc = new JavaSparkContext(conf);
 
+    //Reading input file and save to cache
     JavaRDD<String> docs = sc.textFile(args[0]).cache().repartition(16);
-    long nItems = docs.count();
+    long nDocs = docs.count();
+
+    //Counting the total number of words
+    long nWords = docs.map(doc -> (doc.split(" ")).length).reduce((x, y) -> x + y);
+
 
     /******************************************
      *
@@ -44,55 +49,68 @@ public class G18HM2Davide {
      *
      *******************************************/
 
-    long startDef = System.currentTimeMillis();
-    JavaPairRDD<String, Long> wordcountN = docs
-        .flatMapToPair((document) -> {             // <-- Map phase
-          String[] tokens = document.split(" ");
-          ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
-          for (String token : tokens) {
-            pairs.add(new Tuple2<>(token, 1L));
-          }
-          return pairs.iterator();
-        })
-        .groupByKey()                       // <-- Reduce phase
-        .mapValues((it) -> {
-          long sum = 0;
-          for (long c : it) {
-            sum += c;
-          }
-          return sum;
-        });
 
-    long endDef = System.currentTimeMillis();
+    JavaPairRDD<String, Long> wordcountDef = docs
+      .flatMapToPair((document) -> {             // <-- Map phase
+        String[] tokens = document.split(" ");
+        ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
+        for (String token : tokens) {
+          pairs.add(new Tuple2<>(token, 1L));
+        }
+        return pairs.iterator();
+      })
+      .groupByKey()                       // <-- Reduce phase
+      .mapValues((it) -> {
+        long sum = 0;
+        for (long c : it) {
+          sum += c;
+        }
+        return sum;
+      });
+
     /******************************************
      *
      * Improved Word count 1
      *
      *******************************************/
 
-    Long startWC1 = System.currentTimeMillis();
-
-
     JavaPairRDD<String, Long> wordCountWC1 = docs
-            .flatMapToPair((doc) -> {                  // <-- Map phase
-              String[] tokensArray = doc.split(" ");
-              ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
-              Map<String, Long> wordsCount = Arrays.stream(tokensArray).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-              for(Map.Entry<String, Long> entry: wordsCount.entrySet()) {
-                pairs.add(new Tuple2<String, Long>(entry.getKey(), entry.getValue()));
-              }
-              return pairs.iterator();
-            })
-            .groupByKey()                       // <-- Reduce phase
-            .mapValues((it) -> {
-              long sum = 0;
-              for (long c : it) {
-                sum += c;
-              }
-              return sum;
-            });
+      .flatMapToPair((doc) -> {                                       // <-- Map phase
+        String[] tokensArray = doc.split(" ");
 
-    Long endWC1 = System.currentTimeMillis();
+        //Empty array containing tuples (word, occurrences)
+        ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
+
+        /*
+        For each document count the number of occurrences of every different word.
+        The tokensArray is transformed into a stream object that has been grouped by words,
+        each group of words is counted using the collector Collectors.counting .
+        The result is stored into a map with key the word and value # of occurrences
+         */
+        Map<String, Long> wordsCount = Arrays.stream(tokensArray).collect(Collectors.groupingBy(Function.identity(),
+                Collectors.counting()));
+
+        //Add each entry to pairs array
+        for(Map.Entry<String, Long> entry: wordsCount.entrySet()) {
+          pairs.add(new Tuple2<String, Long>(entry.getKey(), entry.getValue()));
+        }
+
+        return pairs.iterator();
+
+      })
+      .groupByKey()                       // <-- Reduce phase
+      .mapValues((it) -> {
+
+        //summing the number of occurrences
+        long sum = 0;
+
+        for (long c : it) {
+          sum += c;
+        }
+
+        return sum;
+
+      });
 
 
     /******************************************
@@ -101,47 +119,65 @@ public class G18HM2Davide {
      *
      *******************************************/
 
-    long startWC2 = System.currentTimeMillis();
 
     /******* ROUND 1 *******/
-    long sqrtN = (long) Math.ceil(Math.sqrt(nItems));
+    long sqrtN = (long) Math.ceil(Math.sqrt(nWords));
     JavaPairRDD<String, Long> wordCountWC2 = docs
-        .flatMapToPair((String doc) -> {                  // <-- Map phase
-          String[] tokensArray = doc.split(" ");
-          ArrayList<Tuple2<Long, Tuple2<String,Long>>> pairs = new ArrayList<>();
-          Map<String, Long> wordsCount = Arrays.stream(tokensArray).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-          for(Map.Entry<String, Long> entry: wordsCount.entrySet()) {
-            pairs.add(new Tuple2<>((long) (Math.floor(Math.random() * sqrtN)),
-                    new Tuple2<String, Long>((String) entry.getKey(), (long) entry.getValue())));
-          }
-          return pairs.iterator();
-        })
+      .flatMapToPair((String doc) -> {                                    // <-- Map phase
+        String[] tokensArray = doc.split(" ");
+        ArrayList<Tuple2<Long, Tuple2<String,Long>>> pairs = new ArrayList<>();
+
+        /*
+        For each document count the number of occurrences of every different word.
+        The tokensArray is transformed into a stream object that has been grouped by words,
+        each group of words is counted using the collector Collectors.counting .
+        The result is stored into a map with key the word and value # of occurrences
+        */
+        Map<String, Long> wordsCount = Arrays.stream(tokensArray).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        /*
+        Produce the intermediate key (x, (word, occurrences)) where x is a random number in
+        [0, sqrt(N)) and N is the number of words
+         */
+        for(Map.Entry<String, Long> entry: wordsCount.entrySet()) {
+          pairs.add(new Tuple2<>((long) (Math.floor(Math.random() * sqrtN)),
+                  new Tuple2<String, Long>((String) entry.getKey(), (long) entry.getValue())));
+        }
+        return pairs.iterator();
+      })
       .groupByKey()                                     // <-- Reduce phase
       .flatMapToPair((pair) -> {
 
-        //counting words summing over different keys
+        //transforming the iterator into a Stream object
         Stream<Tuple2<String, Long>> streamPairs = StreamSupport.stream(pair._2().spliterator(), false);
+
+        /*
+        Grouping the intermediate pairs with same key x by words and summing the number of occurrences,
+        the result is saved into a map with value (word, occurrencesWithKeyX)
+         */
         Map<String, Long> wordsCount = streamPairs.collect(Collectors.groupingBy((x) -> x._1(), Collectors.summingLong(Tuple2::_2)));
         ArrayList<Tuple2<String,Long>> wordForKey = new ArrayList<>();
+
         for(Map.Entry<String, Long> entry: wordsCount.entrySet()) {
           wordForKey.add(new Tuple2<>(entry.getKey(), entry.getValue()));
         }
+
         return wordForKey.iterator();
+
       });
 
     /******* ROUND 2 *******/
     wordCountWC2 = wordCountWC2
     .groupByKey()
     .mapValues((it) -> {
+
+      //summing the number of occurrences
       long sum = 0;
       for (long c : it) {
         sum += c;
       }
       return sum;
     });
-
-    long endWC2 = System.currentTimeMillis();
-
 
 
     /******************************************
@@ -150,22 +186,25 @@ public class G18HM2Davide {
      *
      *******************************************/
 
-    Long startWCR = System.currentTimeMillis();
-
-
     JavaPairRDD<String, Long> wordCountWCR = docs
-            .flatMapToPair((doc) -> {                  // <-- Map phase
-              String[] tokensArray = doc.split(" ");
-              ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
-              Map<String, Long> wordsCount = Arrays.stream(tokensArray).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-              for(Map.Entry<String, Long> entry: wordsCount.entrySet()) {
-                pairs.add(new Tuple2<String, Long>(entry.getKey(), entry.getValue()));
-              }
-              return pairs.iterator();
-            })
-            .reduceByKey((x, y) -> x + y);
+      .flatMapToPair((doc) -> {                                             // <-- Map phase
+        String[] tokensArray = doc.split(" ");
+        ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
 
-    Long endWCR = System.currentTimeMillis();
+        /*
+        For each document count the number of occurrences of every different word.
+        The tokensArray is transformed into a stream object that has been grouped by words,
+        each group of words is counted using the collector Collectors.counting .
+        The result is stored into a map with key the word and value # of occurrences
+        */
+        Map<String, Long> wordsCount = Arrays.stream(tokensArray).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        for(Map.Entry<String, Long> entry: wordsCount.entrySet()) {
+          pairs.add(new Tuple2<String, Long>(entry.getKey(), entry.getValue()));
+        }
+        return pairs.iterator();
+      })
+      .reduceByKey((x, y) -> x + y);
+
 
 
     /******************************************
@@ -174,41 +213,61 @@ public class G18HM2Davide {
      *
      *******************************************/
 
-    System.out.println("********* Time performances *********");
+      //Ask the user n words to display
+      System.out.println("********* Most occurred words *********");
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Choose the number of most frequent words: ");
+      int n= keyboard.nextInt();
 
-    System.out.println("Simple WordCount elapsed time: " + (endDef - startDef) + " ms");
-    System.out.println("Improved WordCount 1 elapsed time: " + (endWC1 - startWC1) + " ms");
-    System.out.println("Improved WordCount 2 elapsed time: " + (endWC2 - startWC2) + " ms");
-    System.out.println("Word count with reduceByKey elapsed time: " +(endWCR - startWCR) + " ms");
+      //Word count using definition
+      long startWCS = System.currentTimeMillis();
+      List<Tuple2<String, Long>> occurrencesWCS= wordcountDef.top(n, new OccurrencesComparator());
+      long endWCS = System.currentTimeMillis();
 
-    System.out.println("********* Most occurred words *********");
-    Scanner keyboard = new Scanner(System.in);
-    System.out.print("Choose the number of most frequent words: ");
-    int n= keyboard.nextInt();
+      //Word count improved 1
+      long startWC1 = System.currentTimeMillis();
+      List<Tuple2<String, Long>> occurrencesWC1= wordCountWC1.top(n, new OccurrencesComparator());
+      long endWC1 = System.currentTimeMillis();
 
-    List<Tuple2<String, Long>> occurrencesWC1= wordCountWC1.top(n, new WordCountComparator());
-    List<Tuple2<String, Long>> occurrencesWC2= wordCountWC2.top(n, new WordCountComparator());
-    List<Tuple2<String, Long>> occurrencesWCR= wordCountWCR.top(n, new WordCountComparator());
+      //Word count improved 2
+      long startWC2 = System.currentTimeMillis();
+      List<Tuple2<String, Long>> occurrencesWC2= wordCountWC2.top(n, new OccurrencesComparator());
+      long endWC2 = System.currentTimeMillis();
 
-    System.out.println("Improved WordCount 1, most frequent words: " + occurrencesWC1);
-    System.out.println("Improved WordCount 2, most frequent words:  " + occurrencesWC2);
-    System.out.println("Improved WordCount with reduceByKey, most frequent words: " + occurrencesWCR);
+      //Word count using reduceByKey
+      long startWCR = System.currentTimeMillis();
+      List<Tuple2<String, Long>> occurrencesWCR= wordCountWCR.top(n, new OccurrencesComparator());
+      long endWCR = System.currentTimeMillis();
 
-    System.out.println("Press Enter to terminate: ");
+      //Printing of the n most frequent words and the results about the timing of the different algorithms
+      System.out.println("********* MOST FREQUENT WORDS ********");
+      System.out.println("Top "+n+" words with straightforward algorithm: " + occurrencesWCS);
+      System.out.println("Top "+n+" words with improved word count 1 algorithm: " + occurrencesWC1);
+      System.out.println("Top "+n+" words with improved word count 2 algorithm: " + occurrencesWC2);
+      System.out.println("Top "+n+" words with improved word count 1 algorithm and reduceByKey: " + occurrencesWCR);
 
-    try {
-        System.in.read();
-    } catch (Exception e) {
+      System.out.println("\n********* TIME PERFORMANCES ********");
+      System.out.println("Elapsed with time straightforward algorithm: " + (endWCS - startWCS) + " ms");
+      System.out.println("Elapsed with time improved word count 1 algorithm: " + (endWC1 - startWC1) + " ms");
+      System.out.println("Elapsed with time improved word count 2 algorithm: " + (endWC2 - startWC2) + " ms");
+      System.out.println("Elapsed with time improved word count 1 with reduceByKey method: " + (endWCR - startWCR) + " ms");
 
-    }
+      System.out.println("Press Enter to terminate: ");
+
+      try {
+          System.in.read();
+      } catch (Exception e) {
+
+      }
 
 
 
   }
 
+  //Defining the comparator class for the .top method
+  public static class OccurrencesComparator implements Serializable, Comparator<Tuple2<String, Long>>{
 
-  public static class WordCountComparator implements Serializable, Comparator<Tuple2<String, Long>>{
-
+    @Override
     public int compare(Tuple2<String, Long> o1, Tuple2<String, Long> o2) {
       if(o1._2<o2._2)
         return -1;
